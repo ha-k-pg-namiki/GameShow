193639682198c9dd8421325f8e247e40af4f7500
MSI\stkkr <stkkr.1224@gmail.com>
2021-04-05 23:37:56 +0900

    反転ギミックのデータとprefabの追加、ChangeGravityというスクリプトの追加、ChangeGravityというタグの追加

---
+++ GameShow/Assets/addFiles/ChangeGravity.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeGravity : MonoBehaviour
+{
+    //重力の大きさ
+    float Gravity = 7.0f;
+
+    //重力が反転しているかどうか
+    bool IsReverse = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKeyDown(KeyCode.A))
+        //{
+        //    Physics.gravity = new Vector3(0, 10, 0);
+        //}
+
+        //反転していない重力
+        if (IsReverse == false)
+        {
+            Physics.gravity = new Vector3(0, -Gravity, 0);
+        }
+        //反転している重力
+        if (IsReverse == true)
+        {
+            Physics.gravity = new Vector3(0, Gravity, 0);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //反転ギミックと離れた瞬間の処理
+        if (other.gameObject.tag == "ChangeGravity")
+        {
+            //反転していないなら反転させる
+            if (IsReverse == false)
+            {
+                IsReverse = true;
+            }
+            //反転しているなら元に戻す
+            else if (IsReverse == true)
+            {
+                IsReverse = false;
+            }
+        }
+    }
+}
