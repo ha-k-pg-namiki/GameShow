ba1cc4774ebe38c9940ca3a5ecfe93128d68524f
MSI\Miokiti Jr <mioneko115@gmail.com>
2021-04-09 22:05:31 +0900

    ジャンプを追加。

--- GameShow/Assets/addFiles/ChangeGravity.cs
+++ GameShow/Assets/addFiles/ChangeGravity.cs
@@ -5,22 +5,27 @@ using UnityEngine;
 public class ChangeGravity : MonoBehaviour
 {
     //重力の大きさ
-    float Gravity = 200.0f;
+    float Gravity = 120.0f;
 
     //移動
-    private float Position;
+    private Vector3 Position;
 
     //回転
     [SerializeField]
-    private float Rotation;
+    private Vector3 Rotation;
 
     //重力が反転しているかどうか
     bool IsReverse = false;
 
+    private Rigidbody rb;//  Rigidbodyを使うための変数
+    [SerializeField]
+    private bool Grounded;//  地面に着地しているか判定する変数
+    public float Jumppower;//  ジャンプ力
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        rb = GetComponent<Rigidbody>();//  rbにRigidbodyの値を代入する
     }
 
     // Update is called once per frame
@@ -31,13 +36,21 @@ public class ChangeGravity : MonoBehaviour
         //    Physics.gravity = new Vector3(0, 10, 0);
         //}
 
+        if (Grounded == true)//  もし、Groundedがtrueなら、
+        {
+            if (Input.GetKeyDown(KeyCode.Space))//  もし、スペースキーがおされたなら、  
+            {
+                Grounded = false;//  Groundedをfalseにする
+                rb.AddForce(Vector3.up * Jumppower);//  上にJumpPower分力をかける
+            }
+        }
 
-        transform.Translate(0f, 0f, Position);
+        transform.Translate(Position.x, Position.y, Position.z);
 
-        Position += 0.0001f;
+        Position.z += 0.0001f;
 
         //回転
-        transform.rotation = Quaternion.Euler(0.0f, 0.0f, Rotation);
+        transform.rotation = Quaternion.Euler(Rotation.x, Rotation.y, Rotation.z);
 
         //反転していない重力
         if (IsReverse == false)
@@ -56,7 +69,11 @@ public class ChangeGravity : MonoBehaviour
         //反転ギミックと離れた瞬間の処理
         if (other.gameObject.tag == "ChangeGravity")
         {
-            Rotation += 180.0f;
+            //裏の世界に入った際さかさまになるので調整
+            Rotation.z += 180.0f;
+
+            //Gravity = 1000.0f;
+
             //反転していないなら反転させる
             if (IsReverse == false)
             {
@@ -67,6 +84,16 @@ public class ChangeGravity : MonoBehaviour
             {
                 IsReverse = false;
             }
+        }
+    }
+
+    void OnCollisionEnter(Collision other)//  地面に触れた時の処理
+    {
+        if (other.gameObject.tag == "Ground")//  もしGroundというタグがついたオブジェクトに触れたら、
+        {
+            //Gravity = 100.0f;
+
+            Grounded = true;//  Groundedをtrueにする
         }
     }
 }
