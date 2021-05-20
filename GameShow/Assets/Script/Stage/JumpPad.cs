e6fe61e255d3840b8874e9be14f6a92004735b20
MSI\stkkr <stkkr.1224@gmail.com>
2021-05-21 08:13:53 +0900

    Stage11までのプリセット作成、ジャンプパッドの追加

---
+++ GameShow/Assets/Script/Stage/JumpPad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    //ジャンプする力の定義
+    [SerializeField] private float JumpForce = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //もしプレイヤータグのオブジェクトに触れたら上に押し上げる
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(0, JumpForce, 0, ForceMode.Impulse);
+        }
+    }
+}
